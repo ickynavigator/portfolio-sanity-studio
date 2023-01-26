3f19b88897840415314ad75b3056b8df8ef1d463
Obi Fortune <gabrielobi.of@gmail.com>
2023-01-26 10:50:59 +0100

    switched the contact form to a text area

--- schemas/documents/contactForms.tsx
+++ schemas/documents/contactForms.tsx
@@ -8,7 +8,7 @@ export default defineType({
   fields: [
     defineField({ name: 'name', title: 'Name', type: 'string' }),
     defineField({ name: 'email', title: 'Email', type: 'string' }),
-    defineField({ name: 'message', title: 'Message', type: 'string' }),
+    defineField({ name: 'message', title: 'Message', type: 'text' }),
     defineField({ name: 'reviewed', title: 'Reviewed', type: 'boolean' }),
   ],
 
